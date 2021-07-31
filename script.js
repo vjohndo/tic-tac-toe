59d614ac976ff14a89d28d5408df96ec49dce0d3
John Do <v.john.do@outlook.com>
2021-07-31 19:26:30 +1000

    added win logic

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let isO = true;
+let gameboardState = []
+let gameSize = 3;
 
 // generate gameboard
 const sizeButton = document.querySelector('#gameControls > button')
-
 const generateGameboard = () => {
+    gameboardState = []
     const sizeInput = document.querySelector('#gameControls > input')
 
-    let gameSize = sizeInput.value;
+    gameSize = sizeInput.value;
     let gameboard = document.getElementById('display');
     
     while (gameboard.firstChild) {
@@ -15,17 +17,23 @@ const generateGameboard = () => {
     }
 
     for (row = 1; row <= gameSize; row ++ ) {
+        
+        gameboardState.push([]);
+
         for (col = 1; col <= gameSize; col ++ ) {
+            gameboardState[row-1].push("")
             const newNode =  document.createElement('div');
             newNode.classList.add(`coord`,`r${row}c${col}`);
-            newNode.textContent = `r${row}c${col}`;
+            newNode.dataset.row = `${row}`
+            newNode.dataset.col = `${col}`
+            newNode.textContent = ``;
             gameboard.append(newNode);
         }   
     }
     
     let array = []
     for (i = 0; i < gameSize; i++) {
-        array.push("auto")
+        array.push("1fr")
     }
     gameboard.style.gridTemplateColumns = array.join(' ');
 
@@ -33,18 +41,62 @@ const generateGameboard = () => {
 }
 sizeButton.addEventListener('click', generateGameboard)
 
+const checkWinCases = (rowChosen,colChosen) => {
+
+    // check row
+    let target = gameboardState[rowChosen-1][colChosen-1]
+    let winStates = {
+        rowWin: true,
+        colWin: true,
+        mainDiagonalWin: true,
+        antiDiagonalWin: true,
+    
+    }
+
+    for (let i = 0; i < gameSize; i++) {
+        if (gameboardState[i][colChosen-1] !== target) {
+            winStates.rowWin = false;
+        }
+        if (gameboardState[rowChosen-1][i] !== target) {
+            winStates.colWin = false;
+        }
+        if (gameboardState[i][i] !== target || gameboardState[i][i] === "") {
+            winStates.mainDiagonalWin = false;
+        }
+        if (gameboardState[i][gameSize-i-1] || gameboardState[i][gameSize-i-1] === "") {
+            winStates.antiDiagonalWin = false;
+        }
+    }
+
+    console.log(Object.values(winStates))
+    // return (Object.values(winStates).includes(true)) ? true : false;
+
+    if  {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 // update nodes when clicked.
 const gameMechanics = () => {
     const nodes = document.querySelectorAll('.coord')
 
     for (let node of nodes) {
         node.addEventListener('click', (event) => {
+            const rowChosen = event.target.dataset.row;
+            const colChosen = event.target.dataset.col;
+            
+            // Update mark
             if (!event.target.dataset.mark) {
                 event.target.textContent = (isO) ? "O" : "X";
-                event.target.dataset.mark = event.target.textContent 
+                event.target.dataset.mark = event.target.textContent
+                gameboardState[rowChosen-1][colChosen-1] = event.target.textContent
                 isO = !isO;
-                console.log('This works');
             }
+
+            // Check if win
+            console.log("is game win?", checkWinCases(rowChosen,colChosen), "for", event.target.textContent);
         })
     }
 }
