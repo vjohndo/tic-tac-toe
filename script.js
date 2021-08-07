0bda9c4343f7c7397ab4e6b594b0b634ac754a33
John Do <v.john.do@outlook.com>
2021-08-07 11:04:42 +1000

    Beta version 1.02

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
+// Grab global DOM elements
 const resetButton = document.querySelector('#reset')
 const botButton = document.querySelector('#bot')
-const sizeInput = document.querySelector('#textInput')
+const sizeInput = document.querySelector('#numInput')
 const gamelog = document.querySelector('#gamelog')
 const root = document.documentElement
 const oRadio = document.querySelector('#oRadio')
 const xRadio = document.querySelector('#xRadio')
 
-const xStart = (event) => {
-        oRadio.checked = false
-        generateGameboard();
-}
-const oStart = (event) => {
-        xRadio.checked = false
-        generateGameboard();
-}
-xRadio.addEventListener('click',xStart);
-oRadio.addEventListener('click',oStart);
-
+// Inisialise game variables upon start of game
 oRadio.checked = true;
 let isOTurn = true;
 let gameState = []
@@ -25,9 +16,29 @@ let moveLimit = 9;
 let movesMade = 0;
 let gameCheckResult = false;
 let isGameOver = false;
-let whoWon = "" // takes on "X","O",""
+let whoWon = "" 
 let isBotOn = false;
 
+// Event listeners for radio buttons to alternate and restart the game
+const xStart = (event) => {
+    oRadio.checked = false
+    if (movesMade === 0 || isBotOn) {
+        generateGameboard();
+    }
+    if (isBotOn && !isOTurn) {
+        setTimeout(markBoard(),500);
+    }
+}
+const oStart = (event) => {
+    xRadio.checked = false
+    if (movesMade === 0 || isBotOn) {
+        generateGameboard();
+    }
+}
+xRadio.addEventListener('click',xStart);
+oRadio.addEventListener('click',oStart);
+
+// Allow bot to be toggled if game is not active. 
 const toggleBot = () => {
     if (gameSize === 3) {
         if (movesMade === 0 || isGameOver) {
@@ -39,23 +50,30 @@ const toggleBot = () => {
         }
     }
 }
-
 botButton.addEventListener('click',toggleBot)
 
-
+// Function for determining who if game is starting
 const currentMark = (isOTurn) => (isOTurn)?"O":"X";
 
+// Function to check if all elements in an array are the same
+const arraySameCheck = (arr) => {
+    return arr.every(x => Object.is(arr[0], x));
+}
+
+// Function that updates Bot Button class 
 const updateBotButton = () => {
     if (gameSize !== 3 && isBotOn) {
-        botButton.classList.toggle('botOn');
+        botButton.classList.remove('botOn');
+        isBotOn = false;
     }
-    if (gameSize === 3) {
+    if (gameSize === 3 && !isBotOn) {
         botButton.classList.add('botAvailable');
     } else {
         botButton.classList.remove('botAvailable');
     }
 }
 
+// Inisialise the games state variables upon start.
 const initVariables = () => {
     isGameOver = false;
     isOTurn = oRadio.checked;
@@ -65,10 +83,9 @@ const initVariables = () => {
     gamelog.textContent = `Player's Turn: ${currentMark(isOTurn)}`;
 }
 
-const arraySameCheck = (arr) => {
-    return arr.every(x => Object.is(arr[0], x));
-}
-
+// Checks if there are any win cases
+// Does this by pushing each row/col/diag to an array and checking if same
+// Returns 'DRAW' 'X' 'O' for end game states, otherwise ''
 const gameboardWinCheck = (gameState) => {
     
     let mainDiagToCheck = [];
@@ -108,6 +125,7 @@ const gameboardWinCheck = (gameState) => {
     return ""
 }
 
+// Function that removes all node events after game is over.
 const removeNodeEvents = () => {
     const nodes = document.querySelectorAll('.coord')
         for (let node of nodes) {
@@ -117,11 +135,46 @@ const removeNodeEvents = () => {
         }
 }
 
+// Call back function for the adding in the hover effect
+const nodeMouseEnter = (event) => {
+    event.target.textContent = currentMark(isOTurn);
+    event.target.classList.add(currentMark(isOTurn));
+}
+
+// Call back function for removing the hover effect after mouse leaves
+const nodeMouseLeave = (event) => {
+    event.target.textContent = "";
+    event.target.classList.remove(currentMark(isOTurn));
+}
+
+// Removes hover features
+const removeHoverState = (object) => {
+    object.removeEventListener('mouseover', nodeMouseEnter);
+    object.removeEventListener('mouseleave', nodeMouseLeave);
+    object.classList.remove(currentMark(isOTurn));
+}
+
+// Check game result
+const updateGameResult = () => {
+    gameCheckResult = (gameboardWinCheck(gameState));
+    if (gameCheckResult === "O" || gameCheckResult === "X") {
+        gamelog.textContent = `WINNER IS ${currentMark(!isOTurn)}!`;
+        whoWon = currentMark(!isOTurn);
+        isGameOver = true;
+        removeNodeEvents();
+    } else if (movesMade === moveLimit) {
+        gamelog.textContent = `IT'S A DRAW`;
+        isGameOver = true;
+        removeNodeEvents();
+    } else {
+        gamelog.textContent = `Player's Turn: ${currentMark(isOTurn)}`;
+    }
+}
+
+// Call back function that runs if a node is clicked
 const nodeClicked = (event) => {
 
-    event.target.removeEventListener('mouseover', nodeMouseEnter);
-    event.target.removeEventListener('mouseleave', nodeMouseLeave);
-    event.target.classList.remove(currentMark(isOTurn));
+    removeHoverState(event.target);
 
     const rowChosen = event.target.dataset.row;
     const colChosen = event.target.dataset.col;
@@ -138,36 +191,14 @@ const nodeClicked = (event) => {
         // GLOBAL GAME VARIABLE FLIPPED
         isOTurn = !isOTurn;
 
-        gameCheckResult = (gameboardWinCheck(gameState));
-        if (gameCheckResult === "O" || gameCheckResult === "X") {
-            gamelog.textContent = `Game has been won by ${currentMark(!isOTurn)}`;
-            whoWon = currentMark(!isOTurn);
-            isGameOver = true;
-            removeNodeEvents();
-        } else if (movesMade === moveLimit) {
-            gamelog.textContent = `It's a draw`;
-            isGameOver = true;
-            removeNodeEvents();
-        } else {
-            gamelog.textContent = `Player's Turn: ${currentMark(isOTurn)}`;
-        }
+        updateGameResult();
     }
     if (isBotOn && !isOTurn && (movesMade < moveLimit)) {
         markBoard();
     } 
 }
 
-const nodeMouseEnter = (event) => {
-    event.target.textContent = currentMark(isOTurn);
-    event.target.classList.add(currentMark(isOTurn));
-}
-
-const nodeMouseLeave = (event) => {
-    event.target.textContent = "";
-    event.target.classList.remove(currentMark(isOTurn));
-}
-
-
+// As the gameboard is being generated, each node added with event listeners
 const addNodeEvents = () => {
     const nodes = document.querySelectorAll('.coord')
     for (let node of nodes) {
@@ -177,6 +208,7 @@ const addNodeEvents = () => {
     }
 }
 
+// Function to generate the game board
 const generateGameboard = () => {
     
     initVariables();
@@ -220,17 +252,18 @@ const generateGameboard = () => {
     // Make nodes clickable
     addNodeEvents();
 
+    // If the bot is on and it is X's turn run the bot.
     if (isBotOn && !isOTurn) {
         setTimeout(markBoard(),500);
     };
 }
 
+// Generates first board on game start & make enter on num input work
 (() => {
     generateGameboard();
-    gamelog.textContent = `Simulation On`
+    gamelog.textContent = `Welcome...`
     resetButton.addEventListener('click', generateGameboard)
     sizeInput.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {generateGameboard()}
     });
 })(window);
-
